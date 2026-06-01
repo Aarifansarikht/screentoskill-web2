cb8cf8aec8e2f3d8449a6160653ca68e819e4585
Aarifansarikht <86506816+Aarifansarikht@users.noreply.github.com>
2026-06-01 10:56:19 +0530

    update

--- components/sections/FAQs.tsx
+++ components/sections/FAQs.tsx
@@ -44,8 +44,8 @@ export default function FAQs() {
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 lg:gap-14 items-center">
           <div className="lg:col-span-5 relative flex justify-center max-w-sm mx-auto lg:max-w-none w-full">
-            <div className="relative w-full max-w-[280px] sm:max-w-[420px] aspect-[482/650] rounded-2xl overflow-hidden shadow-sm border border-[var(--border)] group">
-              <div className="absolute inset-0 bg-[var(--color-brand)]/10 mix-blend-multiply transition-opacity group-hover:opacity-0" />
+            <div className="relative w-full max-w-[280px] sm:max-w-[520px] aspect-[482/450] rounded-2xl overflow-hidden   group">
+              {/* <div className="absolute inset-0 bg-[var(--color-brand)]/10 mix-blend-multiply transition-opacity group-hover:opacity-0" /> */}
               <Image
                 src="/Mobile-App-FAQ.png"
                 alt="Frequently Asked Questions ScreenToSkill"
